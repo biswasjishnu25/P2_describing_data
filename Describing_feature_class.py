e7adbc667639ea91e7f54cb0f3c645d7269cab1d
biswasjishnu25 <jishnubiswas3@gmail.com>
2023-08-29 16:34:57 +0530

    Decribing without full FC path

--- Describing_feature_class.py
+++ Describing_feature_class.py
@@ -1,6 +1,14 @@
 import arcpy
-majorAttractions_features_path = r"D:\3rd_semester\Programming_for_GIS_III\P2_describing_data\Practical_2_ProProject\02_Describing_Data.gdb\MajorAttractions"
+import os
+file_GDB_path = r"D:\3rd_semester\Programming_for_GIS_III\P2_describing_data\Practical_2_ProProject\02_Describing_Data.gdb"
+majorAttraction_fc_name = "MajorAttractions"
+freeways_fc_name = "Freeways"
+majorAttraction_fc_full_path = os.path.join(file_GDB_path, majorAttraction_fc_name)
+freeWays_path = os.path.join(file_GDB_path, freeways_fc_name)
 
-majorAttraction_desc_obj = arcpy.Describe(majorAttractions_features_path)
+majorAttraction_desc_obj = arcpy.Describe(majorAttraction_fc_full_path)
+freeways_desc_obj = arcpy.Describe(freeWays_path)
+
 print("The type of shape is {}".format(majorAttraction_desc_obj.shapeType))
+print(freeways_desc_obj.shapeType)
 print("Process Complete")
